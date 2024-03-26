264d34d5aef876f5d04968b88be368dae0b7d30d
Abhinay20003 <18891a05e8.abhinaykumar@gmail.com>
2024-03-26 17:14:36 +0530

    Added encryption

--- Seller_BackEnd/SellerRegistration-service/src/main/java/com/SellerRegistrationservice/SellerRegistrationServiceApplication.java
+++ Seller_BackEnd/SellerRegistration-service/src/main/java/com/SellerRegistrationservice/SellerRegistrationServiceApplication.java
@@ -5,10 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 @OpenAPIDefinition(info=@Info(description="Seller Service Documentation"))
 public class SellerRegistrationServiceApplication {
 
